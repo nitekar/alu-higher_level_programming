53354a1c47008c492a1408b52b9bb2f46e9caab9
nitekar <r.niteka@alustudent.com>
2023-11-10 05:54:58 -0800

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return ("None")
-    x = my_list[0]
-    for i in my_list:
-        if i > x:
-            x = i
-    return (x)
-
+    new_list = []
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
