7578613ab7528fd194693a7835d4be418d74aa30
nitekar <r.niteka@alustudent.com>
2023-11-24 02:33:08 -0800

    Task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """
+    # Write an empty class Rectangle that defines a rectangle:
+    # VARIABLE(" "):
+    # rectangle(int): Simple rectangle
+    # Return: Always 0, (Success).
+    """
+
+    """ 'class rectangle' defines the class is like a blueprint or template...
+    for creating objects...
+    while the 'pass' is used as a placeholder when you don't want to add...
+    any code in a block and this is just a sample of a 2D Polygon with a ...
+    ...4-Perpendicular Sides."""
+    pass
